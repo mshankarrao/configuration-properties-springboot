067af24e6200a1bf6b714771bfde172695be5f96
Shankar <mshankarrao1970@gmail.com>
2017-05-30 21:36:27 -0400

    Initial commit

---
+++ src/main/java/com/configuration/properties/ConfigurationPropertiesSpringbootApplication.java
@@ -0,0 +1,22 @@
+package com.configuration.properties;
+
+import com.configuration.properties.config.MyConfig;
+import com.sun.glass.ui.Application;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+@EnableConfigurationProperties
+public class ConfigurationPropertiesSpringbootApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext applicationContext= SpringApplication.run(ConfigurationPropertiesSpringbootApplication.class, args);
+		MyConfig myConfig= (MyConfig) applicationContext.getBean("myConfig");
+		System.out.println(myConfig.toString());
+
+
+
+	}
+}
---
+++ src/main/java/com/configuration/properties/controller/ConfigurationController.java
@@ -0,0 +1,22 @@
+package com.configuration.properties.controller;
+
+import com.configuration.properties.config.MyConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by shankarraomata on 5/30/17.
+ */
+@RestController
+public class ConfigurationController {
+
+    @Autowired
+    private MyConfig myConfig;
+
+    @RequestMapping("/")
+    public String testConfigurationProperties()
+    {
+        return myConfig.getAdminFirstName()+" is from Configuration properties";
+    }
+}
